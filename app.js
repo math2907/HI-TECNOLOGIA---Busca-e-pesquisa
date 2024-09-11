2d8c6ab690895da9f6e5320652b2225e1611bb83
math2907 <matheuslunaqueiroz@gmail.com>
2024-09-10 22:07:48 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+
+const header = document.querySelector('.header');
+let prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+  const currentScrollPos = window.pageYOffset;
+
+  if (prevScrollpos > currentScrollPos) {
+    header.style.top = '0';
+  } else {
+    header.style.top = '-50px'; // Ajuste o valor para controlar a distância
+  }
+
+  prevScrollpos = currentScrollPos;
+}
+
+function pesquisar(){
+
+    // Obtém a seção onde os resultados serão exibidos
+    let section = document.getElementById("resultado-pesquisa");
+    
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+        console.log(campoPesquisa);
+
+        if (!campoPesquisa) {
+            section.innerHTML = "<p style='text-align: center; color: white;' >: Nada foi encontrado. : <br> : Você precisa escrever o nome de um produto. : </p>";
+
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    // Inicializa uma string para armazenar os resultados
+    let resultados = "";
+    let titulo = "";
+    let descricao= "";
+    let tags= "";
+
+    // Itera sobre os dados e cria elementos HTML para cada resultado
+    for(let dado of dados){
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descrição.toLowerCase()
+        tags= dado.tags.toLowerCase()
+
+        if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+
+            //cria um novo elemento
+            console.log(titulo.includes(campoPesquisa))
+
+            resultados += `
+    
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target= "blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta"> ${dado.descrição}</p>
+                <a href="${dado.link}" target="_blank">Mais informações</a>
+            </div>
+            `;
+        }
+       
+    }
+
+if(!resultados){
+    resultados = "<p> : Nenhum produto foi encontrado : <p>"
+
+}
+
+    // Atribui os resultados gerados à seção
+    section.innerHTML = resultados;
+}
---
+++ dados.js
@@ -0,0 +1,28 @@
+let dados = [
+   { titulo: "NEON",
+    descrição: "O NEON é um CLP compacto para diversas utilizações e com alto grau de conectividade. Conta com 6 modelos de módulos de I/O com várias combinações diferentes e recursos. Com ele você tem suporte para até 2 módulos integrados, e podendo conectar até 14 unidades do I/O remoto RION chegando a 240 pontos.",
+    link : "https://www.hitecnologia.com.br/obsoletos/neon/",
+    tags: "clp módulos modulos i/o ",
+},
+{ titulo: "R9X307",
+    descrição: "O módulo R9X307 é um conversor de interface de comunicação serial para wireless. Operando na faixa de 900MHz com uma potência de transmissão de até 1W, uma vez configurado, atua como um canal de comunicação transparente ao conteúdo dos dados.",
+    link : "https://www.hitecnologia.com.br/produto/r9x307/",
+    tags: "conversor wireless comunicação comunicacao",
+},
+{ titulo: "HIstudio",
+    descrição: "O HIstudio é um ambiente de software desenvolvido para a programação, configuração, depuração e documentação de programas dos controladores da HI Tecnologia. Disponibiliza as linguaguens de programação ST (Texto Estruturado) e LD (Ladder), com bibliotecas de funções aderente a norma IEC 61131-3 e suporte para criação de funções e blocos de funções.",
+    link : "https://www.hitecnologia.com.br/produto/histudio/",
+    tags: " HI ambiente programacao programação depuracao depuração st ladder norma funcoes funções",
+    
+},
+{ titulo: "MMI700",
+    descrição: "A MMI700 é uma Interface Homem-Máquina para ambientes industriais, desenvolvidas para integração com CLPs e equipamentos de supervisão e controle com foco em aplicações que necessitam de hardware com melhor custo benefício e que não possuem um elevado número de parâmetros de usuário para serem monitorados ou programados.",
+    link : "https://www.hitecnologia.com.br/produto/mmi700/",
+    tags: "IHM interface teclado clp",
+},
+
+    
+];
+
+
+    
